20ccc4b13b02a3fe9d5ebbb02d64a219879d6b08
codabytez <chidiobinna0001@gmail.com>
2023-12-11 18:57:08 +0100

    resume opens in a new tab

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
-const resume = "https://docs.google.com/document/d/1JJnujuxHniwqojjNk8hRnvtatKwmiNloD_8l1GWSA5Y/edit?usp=sharing";
+const resume =
+  "https://docs.google.com/document/d/1JJnujuxHniwqojjNk8hRnvtatKwmiNloD_8l1GWSA5Y/edit?usp=sharing";
 const Profile = () => {
   return (
     <div className="py-4">
@@ -6,6 +7,7 @@ const Profile = () => {
       <p className="text-center pt-2 text-sm font-light">Software Engineer</p>
       <a
         href={resume}
+        target="_blank"
         className="mt-4 py-2 px-4 text-sm rounded-xl border border-secondary flex w-max justify-center text-center m-auto transition duration-300 hover:bg-secondary hover:text-primary select-none"
       >
         VIEW RESUME
